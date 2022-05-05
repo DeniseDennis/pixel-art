e40273950aee88cbb7d396fba4dfc58582c847f7
Denise <denisedennis629@gmail.com>
2022-05-04 23:24:27 -0400

    fix: Add descriptive comments
    Add descriptive comments for variables. Add metadata

--- designs.js
+++ designs.js
@@ -1,5 +1,4 @@
-// Select color input
-// Select size input
+// Establishes readable variables for html ids
 const height = document.getElementById('inputHeight');
 const width = document.getElementById('inputWidth');
 const fullPic = document.getElementById('pixelCanvas');
@@ -11,23 +10,29 @@ const colorPicker = document.getElementById('colorPicker');
 
 // When size is submitted by the user, call makeGrid()
 sizePicker.addEventListener('submit', function() {
-  event.preventDefault();
-  fullPic.innerHTML = "";
-  makeGrid();
+	event.preventDefault();
+	//resets canvas if one already on page
+	fullPic.innerHTML = "";
+	makeGrid();
 })
 
+/**
+ * @desc Takes users input to create grid of squares
+ * @param row - number of squares representing the height of the grid
+ * @param square - number of squares representing the width of the grid
+ */
 
 function makeGrid() {
-  for(var r = 0; r < height.value; r++) {
-    const row = fullPic.insertRow(r);
-    for(var c = 0; c < width.value; c++){
-      const square = row.insertCell(c);
-      square.onclick = addColor;
-    }
-  }
+	for (var r = 0; r < height.value; r++) {
+		const row = fullPic.insertRow(r);
+		for (var c = 0; c < width.value; c++) {
+			const square = row.insertCell(c);
+			square.onclick = addColor;
+		}
+	}
 }
 
-
+// changes background of item based on color chosen by user
 function addColor() {
-  this.style.background = colorPicker.value;
+	this.style.background = colorPicker.value;
 }
